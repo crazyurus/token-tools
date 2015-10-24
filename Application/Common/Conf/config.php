5fe38a91d82eaba324c033e852d0b1a7e0864cc8
Crazy Urus <crazyurus@qq.com>
2015-10-24 15:54:24 +0800

    添加数据库配置

--- Application/Common/Conf/config.php
+++ Application/Common/Conf/config.php
@@ -6,4 +6,13 @@ return array(
 	    '/^[A-Za-z0-9]{6}$/' => 'Url/Index/index',
 	    ''                   => 'Home/Index/index'
 	),
+
+	// 数据库
+    'DB_TYPE'               =>  'mysql',     // 数据库类型
+    'DB_HOST'               =>  'localhost', // 服务器地址
+    'DB_NAME'               =>  'tokenteam',          // 数据库名
+    'DB_USER'               =>  'root',      // 用户名
+    'DB_PWD'                =>  'root',          // 密码
+    'DB_PORT'               =>  '3306',        // 端口
+    'DB_PREFIX'             =>  '',    // 数据库表前缀
 );
